ac8a3bc42b5e87eedfb5e5416dce5c02f78868ef
Quang14112k <81678733+Quang14112k@users.noreply.github.com>
2021-04-06 13:10:08 +0700

    Update Lab10.sol

--- Lab10.sol
+++ Lab10.sol
@@ -1,47 +1,41 @@
 pragma solidity ^0.5.13;
- contract MappingStruct {
-     
-     struct Payment {
-         uint amount;
-         uint timestamps;
-     }
-     
-     struct balance{
-         uint totalBalance;
-         uint numPayment;
-         mapping (uint => Payment) Payments;
-     } 
-     
-     
-     
-     
-     
-     mapping(address => balance)public balanceReceived;
-     
-        function getBalance() public view returns(uint){
-            return address(this).balance;
-        }
+
+contract MappingStruct {
+    
+    struct Payment{
+        uint amount;
+        uint timestamps;
+    }
+    
+    struct Balance{
+        uint totaBalance;
+        uint numPayments;
+        mapping (uint => Payment) payments;
+    }
+    
+    mapping (address => Balance) public balanceReceived;
+    
+    function getBlance() public view returns(uint){
+        return address(this).balance;
+    }
+    function sendMoney()public payable{
+        balanceReceived[msg.sender].totaBalance += msg.value;
+        Payment memory payment =Payment(msg.value, now);
+        balanceReceived[msg.sender].payments[balanceReceived[msg.sender].numPayments]=payment;
+        balanceReceived[msg.sender].numPayments ++;
         
-        
-        function sendMoney()public payable{
-            balanceReceived[msg.sender].totalBalance += msg.value;
-            
-            Payment memory payment =  Payment(msg.value, now);
-            balanceReceived[msg.sender].payments[balanceReceived[msg.sender].numPayments] = payment;
-            balanceReceived[msg.sender].numPayments ++;
-            
-        }
-        
-        function withDrawMoney(address payable _to,uint _amount) public{
-            require(balanceReceived[msg.sender] >= _amount,"not enough funds");
-            balanceReceived[msg.sender] -= _amount;
-            _to.transfer(_amount);
-        }
-        
-        function withDrawAllMoney(address payable _to) public{
-            uint balanceToSend = balanceReceived[msg.sender];
-            balanceReceived[msg.sender]=0;
-            _to.transfer(balanceToSend);
-        }
-        
- }
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(balanceReceived[msg.sender].totaBalance >= _amount,"Not enough funds");
+        balanceReceived[msg.sender].totaBalance -= _amount;
+        _to.transfer(_amount);
+    }
+    
+    function withdrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceReceived[msg.sender].totaBalance;
+        balanceReceived[msg.sender].totaBalance = 0;
+        _to.transfer(balanceToSend);
+    }
+    
+}
